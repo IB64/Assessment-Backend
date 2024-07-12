6130b368f2c3ccf77d9251a54181ab60fd3c0fab
Angelo Beleno <trainee.angelo.beleno@sigmalabs.co.uk>
2024-07-12 11:10:47 +0100

    task 1 done

--- marine_experiments/api.py
+++ marine_experiments/api.py
@@ -3,7 +3,8 @@
 from datetime import datetime
 
 from flask import Flask, jsonify, request
-from psycopg2 import sql
+from psycopg2 import sql, extras
+from psycopg2.extensions import connection
 
 from database_functions import get_db_connection
 
@@ -17,6 +18,42 @@ For testing reasons; please ALWAYS use this connection.
 """
 conn = get_db_connection("marine_experiments")
 
+
+def get_all_subjects(conn: connection) -> dict:
+    """Gets all stories from the database and returns it as a python dict."""
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    cur.execute("""
+                SELECT 
+                subject_id,
+                subject_name,
+                species_name,
+                TO_CHAR(date_of_birth, 'YYYY-MM-DD') AS date_of_birth
+                FROM subject
+                INNER JOIN species ON subject.species_id = species.species_id;
+                """)
+    result = cur.fetchall()
+    cur.close()
+    return result
+
+@app.route("/subject", methods=["GET"])
+def get_subjects():
+    # Sort subjects by date_of_birth in descending order
+    subjects = get_all_subjects(conn)
+    sorted_subjects = sorted(subjects, key=lambda x: datetime.strptime(x['date_of_birth'], '%Y-%m-%d'), reverse=True)
+    return jsonify(sorted_subjects)
+
+
+@app.route("/experiment", methods=["GET", "POST"])
+def get_experiments():
+    pass
+
+@app.route("/stories/<int:story_id>", methods=["PATCH", "DELETE"])
+def delete_experiment():
+    pass
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Hello World"
 
 if __name__ == "__main__":
 
